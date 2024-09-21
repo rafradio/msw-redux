9f35147397b94e7c459cc174fa8245a722d2b774
Rafail Abdyushev <rafaelab@mail.ru>
2024-09-21 10:35:29 +0300

    src

---
+++ src/api/server.js
@@ -0,0 +1,26 @@
+import { HttpResponse, http } from "msw";
+import { setupWorker } from "msw/browser";
+
+// response resolver
+const postsResolver = () => {
+  return HttpResponse.json([
+    {
+      title:
+        "Что такое генераторы статических сайтов и почему Astro — лучший фреймворк для разработки лендингов",
+      url: "https://habr.com/ru/articles/779428/",
+      author: "@AlexGriss",
+    },
+    {
+      title: "Как использовать html-элемент <dialog>?",
+      url: "https://habr.com/ru/articles/778542/",
+      author: "@AlexGriss",
+    },
+  ]);
+};
+
+// request resolver
+const postsHandler = http.get("/api/posts", postsResolver);
+
+const handlers = [postsHandler];
+
+export const worker = setupWorker(...handlers);
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { createRoot } from 'react-dom/client'
+import {RouterProvider} from "react-router-dom";
+import {Provider} from "react-redux";
+import App from './App.jsx'
+import './index.css'
+import { store } from "./store/store.js";
+import { router } from "./routes/routes.jsx";
+
+async function enableMocking() {
+  if (process.env.NODE_ENV === "development") {
+    const { worker } = await import("./api/server");
+
+    return worker.start();
+  }
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={router}/>
+      </Provider>
+    </React.StrictMode>,
+  )
+})
